3dcbdac067c85b5ab63786424515e916f74dc62a
neil <win10@neilpang.com>
2021-05-23 23:09:05 +0800

    fix

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ async function download() {
     await exec.exec("tar -xzf " + path.join(workingDir, "./cf." + ext));
     await io.mv("cloudflared", path.join(workingDir, "cloudflared"));
   } else if (link === NGROK_MAC) {
-    await exec.exec("7za e -y " + path.join(workingDir, "./cf." + ext) + "  -o " + workingDir);
+    await exec.exec("7za e -y " + path.join(workingDir, "./cf." + ext) + "  -o" + workingDir);
   } else {
     await exec.exec("unzip " + path.join(workingDir, "./cf." + ext) + "  -d " + workingDir);
   }
